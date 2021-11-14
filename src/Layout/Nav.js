766622d32d32d2eed6061a2d7a1586336a0f87bf
Camilo Garcia <cgarcia369@misena.edu.co>
2021-11-14 13:56:42 -0500

    refactor: se refactoriza nav , se agrega trabajo con navigation

--- src/Layout/Nav.js
+++ src/Layout/Nav.js
@@ -4,17 +4,17 @@ import {
   HStack,
   Slide,
   HamburgerIcon,
-  ArrowBackIcon,
+  Pressable,
   VStack,
   Button,
   Text,
   Image,
   CloseIcon,
 } from 'native-base';
-import {Dimensions} from 'react-native';
+import {Dimensions, TouchableOpacity} from 'react-native';
 const windowHeight = Dimensions.get('window').height;
 const windowWidth = Dimensions.get('window').width;
-const Nav = () => {
+const Nav = ({state, descriptors, navigation, position}) => {
   const [openNav, setOpenNav] = useState(false);
   return (
     <>
@@ -63,24 +63,78 @@ const Nav = () => {
         alignItems={'center'}
         overflow={'hidden'}
         position={'relative'}>
-        <HStack
-          justifyContent={'center'}
-          alignItems={'center'}
-          w={'full'}
-          h={'full'}
-          position={'absolute'}
-          zIndex={10}>
-          <Text ml={'5'} fontSize={'3xl'} color={'white'} fontWeight={'light'}>
-            Hoteles BB
-          </Text>
-          <Box flexGrow={1} alignItems={'flex-end'} justifyContent={'flex-end'}>
-            <Button
-              bgColor={'transparent'}
-              onPress={() => setOpenNav(!openNav)}>
-              <HamburgerIcon color={'warning.300'} size={'10'} />
-            </Button>
-          </Box>
-        </HStack>
+        <VStack w={'full'} h={'full'} position={'absolute'} zIndex={10}>
+          <HStack w={'full'} justifyContent={'center'} alignItems={'center'}>
+            <Text
+              ml={'5'}
+              fontSize={'3xl'}
+              color={'white'}
+              fontWeight={'light'}>
+              Hoteles BB
+            </Text>
+            <Box
+              flexGrow={1}
+              alignItems={'flex-end'}
+              justifyContent={'flex-end'}>
+              <Button
+                bgColor={'transparent'}
+                onPress={() => setOpenNav(!openNav)}>
+                <HamburgerIcon color={'warning.300'} size={'9'} />
+              </Button>
+            </Box>
+          </HStack>
+          <HStack
+            w={'full'}
+            flex={1}
+            mb={'2'}
+            justifyContent={'center'}
+            alignItems={'center'}>
+            {state.routes.map((route, index) => {
+              const {options} = descriptors[route.key];
+              const label =
+                options.tabBarLabel !== undefined
+                  ? options.tabBarLabel
+                  : options.title !== undefined
+                  ? options.title
+                  : route.name;
+
+              const isFocused = state.index === index;
+
+              const onPress = () => {
+                const event = navigation.emit({
+                  type: 'tabPress',
+                  target: route.key,
+                });
+
+                if (!isFocused && !event.defaultPrevented) {
+                  navigation.navigate(route.name);
+                }
+              };
+
+              const onLongPress = () => {
+                navigation.emit({
+                  type: 'tabLongPress',
+                  target: route.key,
+                });
+              };
+
+              return (
+                <Pressable
+                  onPress={onPress}
+                  onLongPress={onLongPress}
+                  flex={1}
+                  bg={'transparent'}>
+                  <Text
+                    fontSize={'xl'}
+                    color={isFocused ? 'orange.300' : 'white'}
+                    textAlign={'center'}>
+                    {label}
+                  </Text>
+                </Pressable>
+              );
+            })}
+          </HStack>
+        </VStack>
         <Image
           position={'absolute'}
           w={'full'}
---
+++ src/screen/LoginScreen.js
@@ -0,0 +1,12 @@
+import {NativeBaseProvider, Text, View} from 'native-base';
+import React from 'react';
+import Nav from './../Layout/Nav';
+const LoginScreen = () => {
+  return (
+    <View justifyContent={'center'} alignItems={'center'} flex={1}>
+      <Text>sdf</Text>
+    </View>
+  );
+};
+
+export default LoginScreen;
