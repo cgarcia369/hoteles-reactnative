09b4235fb722a91ef0d934ec11c39a47060ce273
Camilo Garcia <cgarcia369@misena.edu.co>
2021-11-25 13:01:11 -0500

    chore: se remueve react-router , y se instala typescript y el tipado de react , se inicializa ts.config

---
+++ App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {NativeBaseProvider, Box, Text} from 'native-base';
+export default function App() {
+  return (
+    <NativeBaseProvider>
+      <Box>
+        <Text>holas</Text>
+      </Box>
+    </NativeBaseProvider>
+  );
+}
